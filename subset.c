616a742d6fb335be48a987348f934c0483bd40d6
gaochao <gaochao@oneniceapp.com>
2017-09-28 20:23:51 +0800

    update

--- subset.c
+++ subset.c
@@ -3,14 +3,14 @@
 #include <stdlib.h>  
 #include <math.h>  
   
-
+//n个元素中取m个数
 void combine(int n,int m,int a[],int b[],const int M)
 {
     for(int j = n; j >= m; j--)
     {
         b[m - 1] = j - 1;
         if (m > 1)
-            combine(j-1,m-1,a,b,M);//用到了递归思想
+            combine(j-1,m-1,a,b,M);//用到了递归思想，n-1个元素中取m-1个数
         else
         {
             for (int i = M - 1; i >= 0; i--)
