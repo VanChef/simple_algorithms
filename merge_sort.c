f42f8abd5df0adde2527d0273819c363b67f4c41
gaochao <gaochao@oneniceapp.com>
2017-09-28 12:25:10 +0800

    update merge sort

--- merge_sort.c
+++ merge_sort.c
@@ -7,34 +7,38 @@ void merge_sort(int *out, int size_out, int *in1, int size1, int *in2, int size2
     int value1, value2;
     int ptr1 = 0, ptr2 = 0, out_ptr = 0;
 
-    value1 = in1[ptr1++];
-    value2 = in2[ptr2++];
+    value1 = in1[ptr1];
+    value2 = in2[ptr2];
 
     while(1) {
+        printf("value1 %d, value2 %d\n", value1, value2);
         if (value1 < value2) {
             out[out_ptr ++] = value1;
+            ptr1 ++;
             if (ptr1 >= size1) {
                 break;
             } else {
-                value1 = in1[ptr1++];
+                value1 = in1[ptr1];
             }
-            value1 = in1[ptr1++];
         } else {
+            ptr2 ++;
             out[out_ptr ++] = value2;
-            value2 = in2[ptr2++];
             if (ptr2 >= size2) {
                 break;
             } else {
-                value2 = in2[ptr2++];
+                value2 = in2[ptr2];
             }
         }
     }
 
-    if (ptr1 > size1) {
-        memcpy(&out[out_ptr], &in2[ptr2], size2 - ptr2);
+    printf("%d, %d, %d, %d, %d\n", ptr1, ptr2, size1, size2, out_ptr);
+
+    if (ptr1 >= size1) {
+        printf("out_ptr %d, ptr2 %d, size2 - ptr2 %d\n", out_ptr, ptr2, size2 - ptr2);
+        memcpy(&out[out_ptr], &in2[ptr2], (size2 - ptr2)*sizeof(int));
     }
-    if (ptr2 > size2) {
-        memcpy(&out[out_ptr], &in1[ptr1], size1 - ptr1);
+    if (ptr2 >= size2) {
+        memcpy(&out[out_ptr], &in1[ptr1], (size1 - ptr1)*sizeof(int));
     }
 }
 
