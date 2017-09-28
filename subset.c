f254f84044cf6b13ce216ffac661ce6cd312c93d
gaochao <gaochao@oneniceapp.com>
2017-09-28 20:55:33 +0800

    update

--- subset.c
+++ subset.c
@@ -1,20 +1,20 @@
 
-#include <stdio.h>  
-#include <stdlib.h>  
-#include <math.h>  
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
   
 //n个元素中取m个数
 void combine(int n,int m,int a[],int b[],const int M)
 {
     for(int j = n; j >= m; j--)
     {
-        b[m - 1] = j - 1;
+        b[m - 1] = j - 1;//保存角标j-1
         if (m > 1)
-            combine(j-1,m-1,a,b,M);//用到了递归思想，n-1个元素中取m-1个数
+            combine(j - 1, m - 1, a , b, M);//用到了递归思想，j-1个元素中取m-1个数
         else
         {
             for (int i = M - 1; i >= 0; i--)
-                printf("%d ",a[b[i]]);
+                printf("%d ", a[b[i]]);
             printf("\n");
         }
     }
@@ -22,14 +22,14 @@ void combine(int n,int m,int a[],int b[],const int M)
 
 int main()
 {
-    int n = 10,m = 3;
+    int n = 10, m = 3;
     int b[3];
     int a[10] = {1,2,3,4,5,6,7,8,9,10};  
 
-    for(int i=0;i<n;i++)
+    for(int i = 0; i < n; i++)
         a[i]=i+1;
 
-    const int M=m;
-    combine(n,m,a,b,M);
+    const int M = m;
+    combine(n, m, a, b, M);
 }  
 
