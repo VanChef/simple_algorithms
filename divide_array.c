ff6dd377e8fe39238edfd875a5913c8c434536c9
gaochao <gaochao@oneniceapp.com>
2017-09-29 11:57:42 +0800

    add

---
+++ divide_array.c
@@ -0,0 +1,37 @@
+/* 题目：一个整数数组，将其中的元素分到两个数组中，要求这两个数组的和之差最小
+ *
+ * 思路：类似于背包问题，这个问题要求是尽量填满一个容量为sum/2的背包，
+ * 
+ * 
+ * */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+void avg_divide_array(int *in, int size) {
+
+    int sum = 0;
+    int i;
+
+    for (i = 0; i < sum; i++) {
+        sum += in[i];
+    }
+
+    sum /= 2;
+
+
+}
+
+
+int main()
+{
+
+    int input[] = {10, 5, 4, 3, 2, 9, 11, 12};
+
+
+
+
+
+}
+
