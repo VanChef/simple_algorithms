4704e2b9fe02fbbb9b12f5813bdfe5c7d7d578ee
gaochao <gaochao@oneniceapp.com>
2017-09-30 15:34:42 +0800

    add

---
+++ implement_strstr.c
@@ -0,0 +1,14 @@
+/*
+leetcode题，实现c语言中的库函数strstr
+3.2 Implement strStr()
+  
+Implement strStr().
+Returns a pointer to the first occurrence of needle in haystack, or null if needle is not part of haystack.
+ * 
+
+注：最优方法用到了KMP算法，还需要学习
+
+ * */
+
+char *my_strstr(char *in, char *sub)
+
